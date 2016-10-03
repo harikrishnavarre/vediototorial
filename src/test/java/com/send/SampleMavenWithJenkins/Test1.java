83c661d903666ba1d07e14cedbc923960afd7b64
HARI 2 <HARI@HARI-PC>
2016-10-04 00:46:24 +0530

    add time settings

--- src/test/java/com/send/SampleMavenWithJenkins/Test1.java
+++ src/test/java/com/send/SampleMavenWithJenkins/Test1.java
@@ -1,5 +1,7 @@
 package com.send.SampleMavenWithJenkins;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -11,6 +13,8 @@ public class Test1 {
 	public void f(){
 		
 		WebDriver driver=new FirefoxDriver();
+		
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://docs.seleniumhq.org/download/maven.jsp");
 		
